a98e18a48c07836b91c6d4e13442fa3b02c6514a
ian <ianmugwe656@gmail.com>
2026-05-17 11:28:17 +0300

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:chat_app/core/services/database_service.dart';
 import 'package:chat_app/core/utils/route_utils.dart';
+
 import 'package:chat_app/firebase_options.dart';
 import 'package:chat_app/ui/screens/other/user_provider.dart';
 import 'package:chat_app/ui/screens/splash/splash_screen.dart';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -10,7 +12,10 @@ import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const ChatApp());
 }
@@ -24,6 +29,7 @@ class ChatApp extends StatelessWidget {
       builder: (context, child) => ChangeNotifierProvider(
         create: (context) => UserProvider(DatabaseService()),
         child: const MaterialApp(
+          debugShowCheckedModeBanner: false,
           onGenerateRoute: RouteUtils.onGenerateRoute,
           home: SplashScreen(),
         ),
